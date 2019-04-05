001591b474ab5bdb93638d8a356801c3351a6782
Dairon <dc1094@hotmail.com>
2019-04-05 01:43:16 -0500

    ciclo for
    nuevo ciclo

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
-
-""" a=7
-
-while a==7:
-    print("estoy en el while")
-    a = int(input("ingrese el nuevo valor de a"))
-    if a != 7:
-        break """
-
-
-a=3
-b=4
-
-if a ==3 and b==4:
-    print("es correcto")
+for i in range(0,5):
+    print(i)
